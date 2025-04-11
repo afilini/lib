bd57c3bf561b1a67f9dfbf610481161a42274251
Alekos Filini <alekos.filini@gmail.com>
2025-04-11 10:12:42 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    cursor-arm.url = "github:coder/cursor-arm";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, cursor-arm, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+          ];
+        };
+      }
+    );
+}
