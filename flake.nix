bacde6f25b07cc2677f89e8a7d7dd3b8fa725bdf
Alekos Filini <alekos.filini@gmail.com>
2025-04-20 15:26:46 +0200

    Add backend package to flake.nix

--- flake.nix
+++ flake.nix
@@ -6,9 +6,12 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
     cursor-arm.url = "github:coder/cursor-arm";
+    crane = {
+      url = "github:ipetkov/crane";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, cursor-arm, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, cursor-arm, crane }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -19,8 +22,35 @@
           extensions = [ "rust-src" "rust-analyzer" ];
           targets = [ "wasm32-unknown-unknown" ];
         };
+
+        # Configure crane lib
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          src = ./.;
+          buildInputs = [];
+          nativeBuildInputs = [];
+        };
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build the actual backend package
+        backend = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          pname = "portal-backend";
+          version = "0.1.0";
+          cargoExtraArgs = "--package backend";
+        });
       in
       {
+        packages = {
+          inherit backend;
+          backendTemplates = ./backend/templates;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             rustToolchain
