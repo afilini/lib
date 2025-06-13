48d3c7628d487edf129090f99fac1b13563dc972
Alekos Filini <alekos.filini@gmail.com>
2025-06-13 13:12:30 +0200

    Update react-native example

--- react-native/example/src/App.tsx
+++ react-native/example/src/App.tsx
@@ -1,5 +1,5 @@
 import { View, StyleSheet, Button, TextInput } from 'react-native';
-import { PortalApp, Keypair, parseAuthInitUrl, initLogger, AuthChallengeEvent, type AuthChallengeListener, Mnemonic, generateMnemonic } from 'portal-app-lib';
+import { PortalApp, Keypair, parseAuthInitUrl, initLogger, AuthChallengeEvent, type AuthChallengeListener, Mnemonic, generateMnemonic, AuthResponseStatus, CloseRecurringPaymentResponse, RelayStatus, type PaymentRequestListener, PaymentRequestContent, SinglePaymentRequest, PaymentResponseContent, PaymentStatus, RecurringPaymentRequest, RecurringPaymentResponseContent, RecurringPaymentStatus, type ClosedRecurringPaymentListener } from 'portal-app-lib';
 import { useState } from 'react';
 
 async function main(authInitUrl: string) {
@@ -7,9 +7,9 @@ async function main(authInitUrl: string) {
 
   try {
     // Generate a new random mnemonic
-    const mnemonic = generateMnemonic();
+    // const mnemonic = generateMnemonic();
     // Or load a mnemonic from a string
-    // const mnemonic = new Mnemonic("ocean citizen innocent sheriff kit much involve addict machine fine hill zone");
+    const mnemonic = new Mnemonic("ocean citizen innocent sheriff kit much involve addict machine fine hill zone");
 
     // Get a nostr keypair object (right now we only support master keys)
     const keypair = mnemonic.getKeypair();
@@ -21,20 +21,62 @@ async function main(authInitUrl: string) {
 
     // Setup the listener for auth challenges (login requests)
     class AuthChallengeListenerImpl implements AuthChallengeListener {
-      async onAuthChallenge(event: AuthChallengeEvent): Promise<boolean> {
+      async onAuthChallenge(event: AuthChallengeEvent): Promise<AuthResponseStatus> {
         console.log("Auth challenge received", event);
-        return true;
+
+        // return new AuthResponseStatus.Declined({
+        //   reason: "Not approved",
+        // });
+
+        return new AuthResponseStatus.Approved({
+          grantedPermissions: [],
+          sessionToken: "jwt-like-session-token",
+        });
       }
     }
     app.listenForAuthChallenge(new AuthChallengeListenerImpl());
 
-    // Parse the auth init url
-    const url = parseAuthInitUrl(authInitUrl);
-    console.log(url);
+    class PaymentRequestListenerImpl implements PaymentRequestListener {
+      async onSinglePaymentRequest(event: SinglePaymentRequest): Promise<PaymentResponseContent> {
+        console.log("Single payment request received", event);
+        return {
+          requestId: event.content.requestId,
+          status: new PaymentStatus.Pending(),
+        };
+      }
 
-    // Send the auth init request to the service
-    await app.sendAuthInit(url);
-    console.log("Auth init sent");
+      async onRecurringPaymentRequest(event: RecurringPaymentRequest): Promise<RecurringPaymentResponseContent> {
+        console.log("Recurring payment request received", event);
+        return {
+          requestId: event.content.requestId,
+          status: new RecurringPaymentStatus.Confirmed({
+            authorizedAmount: event.content.amount,
+            authorizedCurrency: event.content.currency,
+            authorizedRecurrence: event.content.recurrence,
+            subscriptionId: "random-subscription-id",
+          }),
+        };
+      }
+    }
+    app.listenForPaymentRequest(new PaymentRequestListenerImpl());
+
+    class ClosedRecurringPaymentListenerImpl implements ClosedRecurringPaymentListener {
+      async onClosedRecurringPayment(event: CloseRecurringPaymentResponse): Promise<void> {
+        console.log("Closed subscription received", event);
+      }
+    }
+    app.listenClosedRecurringPayment(new ClosedRecurringPaymentListenerImpl());
+
+    await app.closeRecurringPayment("npub1ek206p7gwgqzgc6s7sfedmlu87cz9894jzzq0283t72lhz3uuxwsgn9stz", "random-subscription-id");
+
+    // await app.sendAuthInit(parseAuthInitUrl("portal://npub1ek206p7gwgqzgc6s7sfedmlu87cz9894jzzq0283t72lhz3uuxwsgn9stz?relays=wss%3A%2F%2Frelay.damus.io,wss%3A%2F%2Frelay.nostr.net&token=eea752dc-3247-4dfa-8d6d-8304e98b1a75"));
+
+    const status = await app.connectionStatus();
+    status.forEach((value, key) => {
+      if (value === RelayStatus.Connected) {
+        console.log(key);
+      }
+    });
   } catch (error) {
     console.log(error);
   }
